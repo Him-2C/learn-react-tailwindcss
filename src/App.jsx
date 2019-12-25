10c8fb62d6fda9d99ba053c5bb3b15baa0a1f2d3
Him-2C <5635512031@psu.ac.th>
2019-12-25 19:20:39 +0700

    - add tailwindcss - add css nano - add post-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import './assets/app.css';
+import './assets/tailwind.css';
+import './assets/main.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-red-500 text">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,6 +18,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-gray-500"
         >
           Learn React
         </a>
